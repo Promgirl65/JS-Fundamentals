e44453bcba843a8b4ebd57c875fb74311fab03b0
Promgirl65 <olayinkabusolami@gmail.com>
2025-09-16 13:03:36 +0100

    8-square.js

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const size = Number(process.argv[2]);
+
+if (Number.isInteger(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
